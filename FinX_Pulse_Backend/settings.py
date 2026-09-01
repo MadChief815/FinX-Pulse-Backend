ffaa0631768a91b4fdef9e3a2a716e426c3cce1e
Deshan <deshananuruddha815@gmail.com>
2026-09-01 14:44:22 +0530

    Migrated

--- FinX_Pulse_Backend/settings.py
+++ FinX_Pulse_Backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'FinX_Pulse_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finx_pulse',
+        'USER': 'finx_pulse_user',
+        'PASSWORD': 'password!K',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
